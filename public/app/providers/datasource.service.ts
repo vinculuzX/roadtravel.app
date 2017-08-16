31b5ca93faba1cede750523f890d07a52a071d5e
vinculuzx <vinculuzx.app@gmail.com>
2017-08-16 08:07:30 -0300

    folder website

--- public/app/providers/datasource.service.ts
+++ public/app/providers/datasource.service.ts
@@ -7,7 +7,7 @@ import 'rxjs/add/operator/filter'
 @Injectable()
 export class DataSourceService {
   private url:string = "https://api.mlab.com/api/1/databases/"
-  private database:string = "roadtravel_dev"
+  private database:string = "roadtravel"
   private collections:string = "events"
   private token:string = "TABEtV5eHWz9YNwYJIg3t6HIflYBr7FY"
   constructor(private http:Http) {  }
@@ -18,7 +18,7 @@ export class DataSourceService {
       .get( json ,{headers:this.getHeaders()})
       .map((res: Response) => res.json());
   }
-  // getEvent(eventId:string){ 
+  // getEvent(eventId:string){
   //   var json = this.url + this.database + '/collections/'+ this.collections +'/?apiKey=' + this.token
   //   return this.http
   //     .get( json ,{headers:this.getHeaders()})
