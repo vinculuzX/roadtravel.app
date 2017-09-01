f19c83a1ea1dcf321b79f27655874becef78f3fb
vinculuzx <vinculuzx.app@gmail.com>
2017-09-01 15:28:08 -0300

    change layout

--- public/app/pages/front/front.component.ts
+++ public/app/pages/front/front.component.ts
@@ -1,20 +1,27 @@
 import { Component, OnInit } from '@angular/core';
-import {Http,Headers, Response } from '@angular/http';
 import 'rxjs/add/operator/map'
 @Component({
   templateUrl: './front.component.html',
 })
 export class FrontComponent implements OnInit {
   private items:any
-  private apiUrl:string = '../../../api/nav.json'
-  constructor(private _http: Http) {  }
+  public nav = [
+    {
+      title:'Eventos',
+      link:'/events'
+    },
+    {
+      title:'Seja nosso Parceiro',
+      link:'/becameapartner'
+    },
+    {
+      title:'Contato',
+      link:'/contact'
+    },
+
+  ]
+  constructor() {  }
   ngOnInit():void {
-    this.getDatafile().subscribe(items=>{
-      this.items = items
-    })
-  }
-  private getDatafile(){
-    return this._http.get(this.apiUrl)
-    .map((res:Response) => res.json().items)
+
   }
 }
